0f89f6c597a16777960d17930639483dc37d96c5
hogun-mac <hogunhogun@naver.com>
2021-05-30 18:06:57 +0900

    removed tensorboard

--- sac/main.py
+++ sac/main.py
@@ -11,7 +11,7 @@ import numpy as np
 import itertools
 import torch
 from sac import SAC
-from torch.utils.tensorboard import SummaryWriter
+#from torch.utils.tensorboard import SummaryWriter
 from replay_memory import ReplayMemory
 
 crop_min = 19
@@ -94,7 +94,7 @@ np.random.seed(args.seed)
 agent = SAC(observation_space, action_space, args)
 
 #Tesnorboard
-writer = SummaryWriter('runs/{}_SAC_{}_{}_{}'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), args.env_name, args.policy, "autotune" if args.automatic_entropy_tuning else ""))
+#writer = SummaryWriter('runs/{}_SAC_{}_{}_{}'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), args.env_name, args.policy, "autotune" if args.automatic_entropy_tuning else ""))
 
 plt.rc('axes', labelsize=6)
 plt.rc('font', size=6)
@@ -145,11 +145,13 @@ for i_episode in itertools.count(1):
                 # Update parameters of all the networks
                 critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = agent.update_parameters(memory, args.batch_size, updates)
 
+                '''
                 writer.add_scalar('loss/critic_1', critic_1_loss, updates)
                 writer.add_scalar('loss/critic_2', critic_2_loss, updates)
                 writer.add_scalar('loss/policy', policy_loss, updates)
                 writer.add_scalar('loss/entropy_loss', ent_loss, updates)
                 writer.add_scalar('entropy_temprature/alpha', alpha, updates)
+                '''
                 updates += 1
 
         next_state, reward, done, info = env.step(action) # Step
@@ -251,7 +253,7 @@ for i_episode in itertools.count(1):
         avg_reward /= episodes
 
 
-        writer.add_scalar('avg_reward/test', avg_reward, i_episode)
+        #writer.add_scalar('avg_reward/test', avg_reward, i_episode)
 
         print("----------------------------------------")
         print("Test Episodes: {}, Avg. Reward: {}".format(episodes, round(avg_reward, 2)))
