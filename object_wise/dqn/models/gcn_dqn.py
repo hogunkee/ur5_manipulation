283563f89b65f49f8178b5b20d9a2323ee7dea58
hogunkee <hogunhogun@naver.com>
2021-10-01 20:32:47 +0900

    some bugs

--- object_wise/dqn/models/gcn_dqn.py
+++ object_wise/dqn/models/gcn_dqn.py
@@ -8,14 +8,15 @@ from torch.nn.parameter import Parameter
 dtype = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
 
 class GraphConvolution(nn.Module):
-    def __init__(self, in_ch, out_ch, bias=True):
+    def __init__(self, in_ch, out_ch, num_blocks, bias=True):
         super(GraphConvolution, self).__init__()
         self.in_ch = in_ch
         self.out_ch = out_ch
-        self.weight = Parameter(torch.FloatTensor(in_ch, out_ch)).type(dtype)
-        self.root_weight = Parameter(torch.FloatTensor(in_ch, out_ch)).type(dtype)
+        self.num_blocks = num_blocks
+        self.weight = Parameter(torch.FloatTensor(in_ch, out_ch))
+        self.root_weight = Parameter(torch.FloatTensor(in_ch, out_ch))
         if bias:
-            self.bias = Parameter(torch.FloatTensor(in_ch, out_ch)).type(dtype)
+            self.bias = Parameter(torch.FloatTensor(in_ch, out_ch))
         else:
             self.register_parameter('bias', None)
         self.reset_parameters()
@@ -28,10 +29,12 @@ class GraphConvolution(nn.Module):
             self.bias.data.uniform_(-stdv, stdv)
 
     def forward(self, x):
-        adj = torch.ones(x.shape[1], x.shape[1]).type(dtype)
-        support = torch.mm(x, self.weight)
-        out = torch.spmm(adj, support)
-        out += torch.mm(x, self.root_weight)
+        support = torch.matmul(x, self.weight)
+        support = torch.mean(support, 1, keepdim=True)
+        out = torch.matmul(x, self.root_weight)
+        print('out:', out.shape)
+        print('support:', support.shape)
+        out += support
         if self.bias is not None:
             out += self.bias
         return out
@@ -40,42 +43,26 @@ class GraphConvolution(nn.Module):
         return self.__class__.__name__ + f'({self.in_ch} -> {self.out_ch})'
 
 
-class OneblockQ(nn.Module):
-    def __init__(self, n_actions=8, n_hidden=64):
-        super(OneblockQ, self).__init__()
-        self.gcn = nn.Sequential(
-                GraphConvolution(4, n_hidden),
-                nn.ReLU(),
-                GraphConvolution(n_hidden, n_hidden),
-                nn.ReLU(),
-                GraphConvolution(n_hidden, n_actions)
-                )
-
-    def forward(self, x):
-        return self.gcn(x)
-
-
 class ObjectQNet(nn.Module):
-    def __init__(self, n_actions, num_blocks):
+    def __init__(self, n_actions, num_blocks, n_hidden=64):
         super(ObjectQNet, self).__init__()
         self.n_actions = n_actions
         self.num_blocks = num_blocks
-        Q_nets = []
-        for nb in range(self.num_blocks):
-            Q_nets.append(OneblockQ(n_actions=self.n_actions))
-        self.Q_nets = nn.ModuleList(Q_nets)
+        self.gcn = nn.Sequential(
+                GraphConvolution(4, n_hidden, num_blocks),
+                nn.ReLU(),
+                GraphConvolution(n_hidden, n_hidden, num_blocks),
+                nn.ReLU(),
+                GraphConvolution(n_hidden, n_actions, num_blocks)
+                )
 
     def forward(self, state_goal):
         states, goals = state_goal
-        q_values = []
-        bs = states.size()[0]
-        nb = states.size()[1]
-        for i in range(nb):
-            s = states[torch.arange(bs), i] # bs x 2
-            g = goals[torch.arange(bs), i] # bs x 2
-            s_g = torch.cat([s, g], 1) # bs x 4
-            q = self.Q_nets[i](s_g) # bs x 8
-            q = q.unsqueeze(1) # bs x 1 x 8
-            q_values.append(q)
-        return torch.cat(q_values, 1) # bs x nb x 8
+        features = torch.cat([states, goals], -1)
+        print('features:')
+        print(features.shape)
+        q = self.gcn(features)
+        print('q:')
+        print(q.shape)
+        return q
 
