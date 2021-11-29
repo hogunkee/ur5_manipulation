ae9cdcc37976bb3f3ead1b7c59b4e0cf3c19c264
hogunkee <hogunhogun@naver.com>
2021-11-29 19:33:52 +0900

    sdf module

--- object_wise/dqn/sdf_module.py
+++ object_wise/dqn/sdf_module.py
@@ -28,3 +28,30 @@ class UCNModule():
 
         features = self.network(image, None).cpu().detach()
         out_label, selected_pixels = clustering_features(features, num_seeds=100)
+
+        segmap = out_label.cpu().detach().numpy()[0]
+        num_blocks = segmap.max()
+        masks = []
+        for nb in range(num_blocks):
+            _mask = (segmap == nb).astype(float)
+            masks.append(_mask)
+        features = features.numpy()[0]
+        return masks, features
+
+    def get_sdf(self, masks):
+        sdfs = []
+        for seg in masks:
+            sd = skfmm.distance(seg.astype(int) - 0.5, dx=1)
+            sdfs.append(sd)
+        return np.array(sdfs) 
+
+    def get_sdf_features(self, image, data_format='HWC'):
+        masks, features = self.get_masks(image, data_format)
+        sdfs = self.get_sdf(masks)
+
+        block_features = []
+        for sdf in sdfs:
+            local_feature = features[:, sdf>=0]
+            block_features.append(local_feature)
+
+        return sdfs, block_features
