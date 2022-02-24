b09bbfa77aeb33522d07b917fc8cc268a6deae7e
hogunkee <hogunhogun@naver.com>
2022-02-24 21:35:53 +0900

    generate attention

--- object_wise/dqn/models/sdf_gat.py
+++ object_wise/dqn/models/sdf_gat.py
@@ -28,24 +28,26 @@ class GraphAttentionLayer(nn.Module):
         self.leakyrelu = nn.LeakyReLU(self.alpha)
 
     def forward(self, h, adj):
-        Wh = torch.mm(h, self.W) # h.shape: (N, in_features), Wh.shape: (N, out_features)
+        Wh = torch.matmul(h, self.W) # h.shape: (B, N, in_features), Wh.shape: (B, N, out_features)
         e = self._prepare_attentional_mechanism_input(Wh)
 
-        zero_vec = -9e15*torch.ones_like(e)
+        zero_vec = torch.zeros_like(e) #-9e15*torch.ones_like(e)
         attention = torch.where(adj > 0, e, zero_vec)
-        attention = F.softmax(attention, dim=1)
+        attention = attention - torch.where(attention != 0, torch.zeros_like(attention), torch.ones_like(attention) * float('inf'))
+        attention = F.softmax(attention, dim=2)
+        attention = torch.where(adj > 0, attention, zero_vec)
         #attention = F.dropout(attention, self.dropout, training=self.training)
         return attention
 
     def _prepare_attentional_mechanism_input(self, Wh):
-        # Wh.shape (N, out_feature)
+        # Wh.shape (B, N, out_feature)
         # self.a.shape (2 * out_feature, 1)
         # Wh1&2.shape (N, 1)
         # e.shape (N, N)
         Wh1 = torch.matmul(Wh, self.a[:self.out_features, :])
         Wh2 = torch.matmul(Wh, self.a[self.out_features:, :])
         # broadcast add
-        e = Wh1 + Wh2.T
+        e = Wh1 + Wh2.permute([0,2,1])
         return self.leakyrelu(e)
 
     def __repr__(self):
@@ -54,6 +56,7 @@ class GraphAttentionLayer(nn.Module):
 
 class GraphConvolutionLayer(nn.Module):
     def __init__(self, in_ch, out_ch, bias=True):
+        super(GraphConvolutionLayer, self).__init__()
         self.conv_root = nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1) 
         self.conv_inscene = nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1) 
         self.conv_goal = nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1) 
@@ -80,14 +83,13 @@ class GraphConvolutionLayer(nn.Module):
 
 
 class SDFGATQNet(nn.Module):
-    def __init__(self, num_blocks, sdim=1, fdim=12, hdim=64, n_actions=8, n_hidden=16):
+    def __init__(self, num_blocks, sdim=1, fdim=12, hdim=64, n_actions=8):
         super(SDFGATQNet, self).__init__()
         self.n_actions = n_actions
         self.num_blocks = num_blocks
         self.fdim = fdim
-        self.normalize = norm
 
-        self.zeropad = nn.ZeroPad2d((0, 0, num_blocks, num_blocks))
+        self.zeropad = nn.ZeroPad2d((0, num_blocks, 0, num_blocks))
         self.attn_inscene = GraphAttentionLayer(fdim, hdim)
         self.attn_goal = GraphAttentionLayer(fdim, hdim)
         self.cnn1 = nn.Sequential(
@@ -115,14 +117,15 @@ class SDFGATQNet(nn.Module):
         self.gconv2 = GraphConvolutionLayer(8*hdim, 8*hdim)
 
     def generate_adj(self, nb_st, nb_g):
+        NB = self.num_blocks
         adj_in = []
-        adj_g
+        adj_g = []
         for ns, ng in zip(nb_st, nb_g):
-            _adj_in = torch.zeros(self.num_blocks, self.num_blocks)
-            _adj_g = torch.zeros(2*self.num_blocks, 2*self.num_blocks)
-            _adj_in[:ns, :ns] = 1.
-            _adj_g[:ng, self.num_blocks:self.num_blocks+ns] = 1.
-            _adj_g[self.num_blocks:self.num_blocks+ng, :ns] = 1.
+            _adj_in = torch.zeros(NB, NB)
+            _adj_g = torch.zeros(2*NB, 2*NB)
+            _adj_in[:ns, :ns] = torch.ones(ns, ns) - torch.eye(int(ns)) # without self-loop
+            _adj_g[:ns, NB:NB+ng] = 1.
+            #_adj_g[NB:NB+ng, :ns] = 1. # directed graph (g->st:X)
             adj_in.append(_adj_in.unsqueeze(0))
             adj_g.append(_adj_g.unsqueeze(0))
         adj_inscene = torch.cat(adj_in, 0).to(device)
@@ -137,11 +140,9 @@ class SDFGATQNet(nn.Module):
         sdfs_g, feature_g = obs_g
         
         adj_inscene, adj_goal = self.generate_adj(nb_st, nb_g)
-        #print(adj_inscene, adj_goal)
         attn_inscene = self.attn_inscene(feature_st, adj_inscene)
         attn_inscene = self.zeropad(attn_inscene)
         attn_goal = self.attn_goal(torch.cat([feature_st, feature_g], 1), adj_goal)
-        #print(attn_inscene, attn_goal)
         return attn_inscene, attn_goal
 
         #x = self.cnn1
