c771f1bfc5d736acdd20912fb695deb71bf12c19
hogun-mac <hogunhogun@naver.com>
2023-07-16 19:26:02 +0900

    collecting data

--- object_wise/collect_tidying_data.py
+++ object_wise/collect_tidying_data.py
@@ -14,108 +14,70 @@ import multiprocessing as mp
 from multiprocessing import Process
 
 
-def get_action_near_blocks(env, pad=0.06):
-    poses, _ = env.get_poses()
-    obj = np.random.randint(len(poses))
-    pose = poses[obj]
-    x = np.random.uniform(pose[0]-pad, pose[0]+pad)
-    y = np.random.uniform(pose[1]-pad, pose[1]+pad)
-    py, px = env.pos2pixel(x, y)
-    theta = np.arctan2(x-pose[0], y-pose[1]) / np.pi + 1
-    ptheta = np.round((theta + np.random.normal(0, 0.5)) * 4) % 8
-    # ptheta = np.random.randint(8)
-    return [px, py, ptheta]
-
 def collect_npy(process_id, args):
     render = args.render
+    small = args.small
     num_blocks = args.num_blocks
+    num_scenes = args.num_scenes
+    num_sortings = args.num_sortings
+    save_freq = args.save_freq
     camera_height = args.camera_height
     camera_width = args.camera_width
-    small = args.small
     gpu = args.gpu
 
     np.random.seed(process_id)
+    if process_id%2 == 0:
+        dataset = 'train1'
+    else:
+        dataset = 'train2'
     env = UR5Env(render=render, camera_height=camera_height, camera_width=camera_width, \
                  control_freq=5, data_format='NHWC', gpu=gpu, camera_depth=True, small=small,
-                 camera_name='rlview2')
+                 camera_name='rlview2', dataset=dataset)
     env = objectwise_env(env, num_blocks=num_blocks)
 
-    save_freq = args.save_freq
-    num_ep = args.num_ep
-
-    buff_states = []
-    buff_nextstates = []
-    buff_actions = []
-    buff_frames = []
-    buff_nextframes = []
-    buff_rewards = []
-    buff_dones = []
-    log_success = []
-
-    for i in range(100):
+    buff_images = []
+    buff_poses = []
+    buff_rotations = []
+    for n in range(num_scenes):
         env.env.select_objects(num_blocks, -1)
-        env.init_scene()
-    exit()
+        images, poses, rotations = [], [], []
+        for ns in range(num_sortings):
+            img, p, r = env.init_scene()
+            images.append(img)
+            poses.append(p)
+            rotations.append(r)
+        for i in range(len(images)):
+            buff_images.append(images[i])
+            buff_poses.append(poses[i])
+            buff_rotations.append(rotations[i])
 
-    state = env.reset()
-    while len(buff_states) < args.num_data:
-        action = [np.random.randint(env.num_blocks), np.random.randint(env.num_bins)]
-        #action = get_action_near_blocks(env)
-        next_state, reward, done, info = env.step(action)
-        log_success.append(info['success'])
+        if len(buff_images)%save_freq==0:
+            num_files = len([f for f in os.listdir(args.data_dir) if 'image_' in f])
+            i_filename = os.path.join(args.data_dir, 'image_%d.npy' % num_files)
+            p_filename = os.path.join(args.data_dir, 'pose_%d.npy' % num_files)
+            r_filename = os.path.join(args.data_dir, 'rotation_%d.npy' % num_files)
 
-        buff_states.append(state[0])
-        buff_nextstates.append(next_state[0])
-        buff_frames.append(state[1])
-        buff_nextframes.append(next_state[1])
-        buff_actions.append(action)
-        buff_rewards.append(reward)
-        buff_dones.append(int(done))
+            np.save(i_filename, np.array(buff_images))
+            np.save(p_filename, np.array(buff_poses))
+            np.save(r_filename, np.array(buff_rotations))
+            print('Saved %s-th batch.'num_files)
 
-        state = next_state
-        if done:
-            state = env.reset()
+            buff_images.clear()
+            buff_poses.clear()
+            buff_rotations.clear()
 
-        if len(buff_states)%save_freq==0:
-            num_files = len([f for f in os.listdir(args.data_dir) if 'reward_' in f])
-            s_filename = os.path.join(args.data_dir, 'state_%d.npy' % num_files)
-            ns_filename = os.path.join(args.data_dir, 'nextstate_%d.npy' % num_files)
-            a_filename = os.path.join(args.data_dir, 'action_%d.npy' % num_files)
-            r_filename = os.path.join(args.data_dir, 'reward_%d.npy' % num_files)
-            d_filename = os.path.join(args.data_dir, 'done_%d.npy' % num_files)
-            f_filename = os.path.join(args.data_dir, 'frame_%d.npy' % num_files)
-            nf_filename = os.path.join(args.data_dir, 'nextframe_%d.npy' % num_files)
-
-            np.save(s_filename, np.array(buff_states))
-            np.save(ns_filename, np.array(buff_nextstates))
-            np.save(f_filename, (np.array(buff_frames)*255).astype(np.uint8))
-            np.save(nf_filename, (np.array(buff_nextframes)*255).astype(np.uint8))
-            np.save(a_filename, np.array(buff_actions))
-            np.save(r_filename, np.array(buff_rewards))
-            np.save(d_filename, np.array(buff_dones))
-            print('success rate:', np.sum(log_success)/len(log_success))
-
-            buff_states.clear()
-            buff_nextstates.clear()
-            buff_frames.clear()
-            buff_nextframes.clear()
-            buff_actions.clear()
-            buff_rewards.clear()
-            buff_dones.clear()
-            log_success.clear()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--render", action="store_true")
     parser.add_argument("--small", action="store_true")
     parser.add_argument("--num_blocks", default=3, type=int)
-    parser.add_argument("--num_scenes", default=4, type=int)
+    parser.add_argument("--num_scenes", default=20000, type=int)
+    parser.add_argument("--num_sortings", default=20000, type=int)
     parser.add_argument("--save_freq", default=2048, type=int)
-    parser.add_argument("--num_ep", default=5000, type=int)
     parser.add_argument("--camera_height", default=96, type=int)
     parser.add_argument("--camera_width", default=96, type=int)
     parser.add_argument("--num_process", default=4, type=int)
-    parser.add_argument("--num_data", default=50000, type=int)
     parser.add_argument("--data_dir", default='', type=str)
     parser.add_argument("--gpu", default=-1, type=int)
     args = parser.parse_args()
